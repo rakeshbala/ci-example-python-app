66f5184921e6db0f92106d4bca31e54996979532
Rakesh Balasubramanian <rakesh.balasubramanian@factset.com>
2018-11-06 17:46:09 -0500

    fixup! Set up CI with Azure Pipelines

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,12 +2,6 @@ import sys
 sys.path.append('../src')
 
 import unittest
-from mymatrix import MyMatrix
-
-class TestMyMatrix(unittest.TestCase):
-    def test_get(self):
-        m = MyMatrix(3, 5)
-        self.assertEqual(m.get(1, 1), 6)
 
 if __name__ == "__main__":
     unittest.main()
--- tests/main.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-from mymatrix import MyMatrix
-
-class TestMyMatrix(unittest.TestCase):
-    def test_get(self):
-        m = MyMatrix(3, 5)
-        self.assertEqual(m.get(1, 1), 6)
